aadb0be85077ae7bb42a3b5d87165501819c9ebb
Feng Tian <fengttt@gmail.com>
2017-05-21 20:46:54 -0700

    Gen file into plugin as well.

--- phi/proto/gen.sh
+++ phi/proto/gen.sh
@@ -1,2 +1,3 @@
 protoc --go_out=plugins=grpc:../go/src/vitessedata/phi/proto/xdrive -I . xdrive_data.proto 
+protoc --go_out=plugins=grpc:../../plugin/go/src/vitessedata/proto/xdrive -I . xdrive_data.proto 
 protoc --python_out=../py2/vitessedata/phi -I . xdrive_data.proto
