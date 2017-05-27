4a007a1a23f894795caf8981470fdd05363edb22
Feng Tian <fengttt@gmail.com>
2017-05-27 13:26:52 -0700

    Remove phi, to reduce GOPATH conflicts.

--- phi/proto/gen.sh
+++
@@ -1,3 +0,0 @@
-protoc --go_out=plugins=grpc:../go/src/vitessedata/phi/proto/xdrive -I . xdrive_data.proto 
-protoc --go_out=plugins=grpc:../../plugin/go/src/vitessedata/proto/xdrive -I . xdrive_data.proto 
-protoc --python_out=../py2/vitessedata/phi -I . xdrive_data.proto
--- phi/py2/vitessedata/phi/__init__.py
+++
@@ -1,4 +0,0 @@
-from stream import DeclareTypes
-from stream import NextInput
-from stream import WriteOutput
-from stream import FlushOutput
