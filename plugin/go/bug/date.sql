c36584043871c420963be2167b04c9c20a330d50
Feng Tian <fengttt@gmail.com>
2017-08-06 00:28:13 -0700

    Beef up tests.

--- plugin/go/bug/date.sql
+++ plugin/go/bug/date.sql
@@ -1,28 +1,43 @@
-drop external table if exists datebug;
-create external table datebug(d date) location ('xdrive://localhost:31416/fs/date.csv') format 'csv';
+drop external table if exists dateok; 
+create external table dateok(d date) location ('xdrive://localhost:31416/fs/date.csv') 
+format 'csv';
+
+drop external table if exists datebug; 
+create external table datebug(d date) location ('xdrive://localhost:31416/fs/date.csv') 
+format 'csv' (integer timestamp);
+
+drop external table if exists dateintok;
+create external table dateintok(d date) location ('xdrive://localhost:31416/fs/dateint.csv') 
+format 'csv' (integer timestamp);
 
 drop external table if exists dateintbug;
-create external table dateintbug(d date) location ('xdrive://localhost:31416/fs/dateint.csv') format 'csv';
+create external table dateintbug(d date) location ('xdrive://localhost:31416/fs/dateint.csv') 
+format 'csv';
+
+drop external table if exists myxdateok; 
+create external table myxdateok(d date) location ('xdrive://localhost:31416/myx/date.csv') 
+format 'csv';
 
 drop external table if exists myxdatebug;
-create external table myxdatebug(d date) location ('xdrive://localhost:31416/myx/date.csv') format 'csv';
+create external table myxdatebug(d date) location ('xdrive://localhost:31416/myx/date.csv')
+format 'csv' (integer timestamp);
+
+drop external table if exists myxdateintok; 
+create external table myxdateintok(d date) location ('xdrive://localhost:31416/myx/dateint.csv') 
+format 'csv' (integer timestamp);
 
 drop external table if exists myxdateintbug;
-create external table myxdateintbug(d date) location ('xdrive://localhost:31416/myx/dateint.csv') format 'csv';
+create external table myxdateintbug(d date) location ('xdrive://localhost:31416/myx/dateint.csv') 
+format 'csv';
 
+select 'OK';
+select * from dateok;
+select * from dateintok;
+select * from myxdateok;
+select * from myxdateintok;
 
 select 'Bug bug ...';
 select * from datebug;
-
-select 'This is OK, should it?';
 select * from dateintbug;
-
-select 'Plugin Bug bug ...';
 select * from myxdatebug;
-
-select 'Plugin This is OK, should it?';
 select * from myxdateintbug;
-
-
-
-
--- plugin/go/bug/ts.sql
+++ plugin/go/bug/ts.sql
@@ -1,8 +1,38 @@
+drop external table if exists tsok; 
+create external table tsok(i int, t text, j int, ts timestamp, ts2 timestamptz) 
+location ('xdrive://localhost:31416/fs/ts.csv') 
+format 'csv';
+
 drop external table if exists tsbug; 
-create external table tsbug(i int, t text, j int, ts timestamp, ts2 timestamptz) location ('xdrive://localhost:31416/fs/ts.csv') format 'csv';
+create external table tsbug(i int, t text, j int, ts timestamp, ts2 timestamptz) 
+location ('xdrive://localhost:31416/fs/ts.csv') 
+format 'csv' (integer timestamp);
+
+drop external table if exists myxtsok; 
+create external table myxtsok(i int, t text, j int, ts timestamp, ts2 timestamptz) 
+location ('xdrive://localhost:31416/myx/ts.csv') 
+format 'csv';
 
 drop external table if exists myxtsbug; 
-create external table myxtsbug(i int, t text, j int, ts timestamp, ts2 timestamptz) location ('xdrive://localhost:31416/myx/ts.csv') format 'csv';
+create external table myxtsbug(i int, t text, j int, ts timestamp, ts2 timestamptz) 
+location ('xdrive://localhost:31416/myx/ts.csv') 
+format 'csv' (integer timestamp);
 
+drop external table if exists tsintok;
+create external table tsintok(d timestamp) location ('xdrive://localhost:31416/fs/dateint.csv') 
+format 'csv' (integer timestamp);
+
+drop external table if exists tsintbug;
+create external table tsintbug(d date) location ('xdrive://localhost:31416/fs/dateint.csv') 
+format 'csv';
+
+
+select 'OK';
+select * from tsok; 
+select * from tsintok;
+select * from myxtsok; 
+
+select 'Bug';
 select * from tsbug;
+select * from tsintbug;
 select * from myxtsbug; 
