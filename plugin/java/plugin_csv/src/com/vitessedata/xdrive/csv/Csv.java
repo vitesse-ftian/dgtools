f909ff464b6333ef3ab15cedf0000ef06e3f0847
vderic <ericlam@vitessedata.com>
2018-03-20 03:49:08 +0000

    csv update

--- plugin/java/plugin_csv/src/com/vitessedata/xdrive/csv/Csv.java
+++ plugin/java/plugin_csv/src/com/vitessedata/xdrive/csv/Csv.java
@@ -20,7 +20,6 @@ public class Csv {
     private Xdrive2.WriteRequest wreq;
     private CSVPrinter csvprinter;
     private FileWriter out;
-    private List<String[]> data;
 
     private String base_path;
 
@@ -59,9 +58,193 @@ public class Csv {
     }
 
 
+    public void readfile(Xdrive2.ReadRequest rreq, Path p) throws Exception {
+
+        Reader in = null;
+
+        Xdrive2.XCol.Builder[] xcolb = new Xdrive2.XCol.Builder[coldesc.length];
+        for (int i = 0; i < coldesc.length; i++) {
+            xcolb[i] = Xdrive2.XCol.newBuilder();
+            xcolb[i].setColname(coldesc[i].getName());
+        }
+
+        try {
+            in = new FileReader(p.toFile());
+            int nrow = 0;
+            Iterable<CSVRecord> records = CSVFormat.RFC4180.withNullString("").parse(in);
+            for (CSVRecord record : records) {
+                nrow++;
+                for (int i = 0 ; i < ncol ; i++) {
+                    Xdrive2.ColumnDesc desc = coldesc[i];
+                    switch (Xdrive2.SpqType.forNumber(desc.getType())) {
+                    case BOOL:
+                        {
+                            String str = record.get(i);
+                            if (str == null) {
+                                xcolb[i].addNullmap(true);
+                                xcolb[i].addI32Data(0);
+                            } else {
+
+                                boolean v = Boolean.parseBoolean(str);
+                                xcolb[i].addNullmap(false);
+                                xcolb[i].addI32Data(v ? 1: 0);
+
+                            }
+                        }
+                        break;
+                    case INT16:
+                        {
+                            String str = record.get(i);
+                            if (str == null) {
+                                xcolb[i].addNullmap(true);
+                                xcolb[i].addI32Data(0);
+                            } else {
+                                int s = (int) Short.parseShort(str);
+                                xcolb[i].addNullmap(false);
+                                xcolb[i].addI32Data(s);
+                            } 
+                        }
+                        break;
+                    case INT32:
+                        {
+                            String str = record.get(i);
+                            if (str == null) {
+                                xcolb[i].addNullmap(true);
+                                xcolb[i].addI32Data(0);
+                            } else {
+                                int s = Integer.parseInt(str);
+                                xcolb[i].addNullmap(false);
+                                xcolb[i].addI32Data(s);
+                            } 
+                        }
+                        break;
+                    case INT64:
+                        {
+                            String str = record.get(i);
+                            if (str == null) {
+                                xcolb[i].addNullmap(true);
+                                xcolb[i].addI64Data(0);
+                            } else {
+                                long v = Long.parseLong(str);
+                                xcolb[i].addNullmap(false);
+                                xcolb[i].addI64Data(v);
+                            }
+                        }
+                        break;
+                    case FLOAT:
+                        {
+                            String str = record.get(i);
+                            if (str == null) {
+                                xcolb[i].addNullmap(true);
+                                xcolb[i].addF32Data(0);
+                            } else {
+                                float f = Float.parseFloat(str);
+                                xcolb[i].addNullmap(false);
+                                xcolb[i].addF32Data(f);
+                            } 
+                        }
+                        break;
+                    case DOUBLE:
+                        {
+                            String str = record.get(i);
+                            if (str == null) {
+                                xcolb[i].addNullmap(true);
+                                xcolb[i].addF64Data(0);
+                            } else {
+                                double v = Double.parseDouble(str);
+                                xcolb[i].addNullmap(false);
+                                xcolb[i].addF64Data(v);
+                            } 
+                        }
+                        break;
+                    case DATE:
+                    case TIME_MILLIS:
+                        {
+                            String str = record.get(i);
+                            if (str == null) {
+                                xcolb[i].addNullmap(true);                                
+                                xcolb[i].addI32Data(0);
+                            } else {
+                                int v = Integer.parseInt(str);
+                                xcolb[i].addNullmap(false);
+                                xcolb[i].addI32Data(v);
+                            } 
+                        }
+                        break;
+                         
+                    case TIME_MICROS:
+                    case TIMESTAMP_MILLIS:
+                    case TIMESTAMPTZ_MILLIS:
+                    case TIMESTAMP_MICROS:
+                    case TIMESTAMPTZ_MICROS:
+                        {
+                            String str = record.get(i);
+                            if (str == null) {
+                                xcolb[i].addNullmap(true);                                
+                                xcolb[i].addI64Data(0);
+                            } else {
+                                long v = Long.parseLong(str);
+                                xcolb[i].addNullmap(false);
+                                xcolb[i].addI64Data(v);
+                            } 
+                        }
+                        break;                     
+   
+                    default:
+                        {
+                            String str = record.get(i);
+                            if (str == null) {
+                                xcolb[i].addNullmap(true);
+                                xcolb[i].addSdata("");
+                            } else {
+                                xcolb[i].addNullmap(false);
+                                xcolb[i].addSdata(str);
+                            }
+                        }
+                        break;
+                    }  // switch
+                } // for col
+                
+                if (nrow == 8192) {
+                    for (int i = 0 ;i < xcolb.length; i++) {
+                        xcolb[i].setNrow(nrow);
+                        XdriveUtil.replyXColData(xcolb[i].build());
+                    }
+                    nrow = 0;
+                    for (int i = 0 ; i < xcolb.length ; i++) {
+                        xcolb[i] = Xdrive2.XCol.newBuilder();
+                        xcolb[i].setColname(coldesc[i].getName());
+                    }
+                }
+
+            } // for records
+
+            // write the residuals
+            if (nrow > 0) {
+                System.err.printf("READ: %d rows.\n", nrow);
+                for (int i = 0; i < xcolb.length; i++) {
+                    xcolb[i].setNrow(nrow);
+                    XdriveUtil.replyXColData(xcolb[i].build());
+                }
+            }
+                
+            
+        } catch (IOException ex) {
+            throw ex;
+        }
+        finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
     public void read(Xdrive2.ReadRequest rreq) throws Exception {
         ncol = rreq.getColumndescCount();
         coldesc = new Xdrive2.ColumnDesc[ncol];
+
+        for (int j = 0 ; j < rreq.getColumndescCount() ; j++) {
+            coldesc[j] = rreq.getColumndesc(j);
+        }
 
         String filepath = rreq.getFilespec().getPath();
         int idx = filepath.indexOf("/", 1);
@@ -74,14 +257,11 @@ public class Csv {
             //Integer hashCode = Math.abs(CommonUtils.hash(fs.getPath().hashCode()));
             int hashCode = Math.abs(p.hashCode());
             if (hashCode % rreq.getFragCnt() == rreq.getFragId()) {
-                Reader in = new FileReader(p.toFile());
-                Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(in);
-                for (CSVRecord record : records) {
-                    record.get(0);
-                    record.get(1);
-                }
+                readfile(rreq, p);
             }
         }
+
+        XdriveUtil.replyXColData(null);
     }
     
     public void sizeMeta(Xdrive2.SizeMetaRequest szreq) throws Exception {
@@ -117,9 +297,8 @@ public class Csv {
             nextcol++;
             if (nextcol == ncol) {
                 // write 
-                csvprinter.printRecords(data);
-                
-                data.clear();
+
+                writeCols();
                 nextcol = 0;
             }
             return 0;
@@ -150,8 +329,136 @@ public class Csv {
         out = new FileWriter(path);
         csvprinter = new CSVPrinter(out, CSVFormat.DEFAULT);
         
+    }
 
+    public void writeCols() throws Exception {
+        int nrow = cols[0].getNrow();
+        
+        for (int row = 0 ; row < nrow; row++) {
+            writeOneRow(row);
+        }
 
     }
 
+    public void writeOneRow(int row) throws Exception {
+        List<String> data = new ArrayList<String>();
+
+        for (int col = 0 ; col < ncol ; col++) {
+            Xdrive2.ColumnDesc desc = wreq.getColumndesc(col);
+            Xdrive2.XCol xcol = cols[col]; 
+
+            switch (Xdrive2.SpqType.forNumber(desc.getType())) {
+
+            case BOOL:
+                {
+                    boolean b = false;
+                    if (!xcol.getNullmap(row)) {
+                        b = xcol.getI32Data(row) != 0;
+                        data.add(Boolean.toString(b));
+                    } else {
+                        data.add("");
+                    }
+                }
+                break;
+
+            case INT16:
+                {
+                    short s = 0;
+                    if (!xcol.getNullmap(row)) {
+                        s = (short) xcol.getI32Data(row);
+                        data.add(Short.toString(s));
+                    } else {
+                        data.add("");
+                    }
+                }
+                break;
+
+            case INT32:
+                {
+                    int i = 0;
+                    if (!xcol.getNullmap(row)) {
+                        i = xcol.getI32Data(row);
+                        data.add(Integer.toString(i));
+                    } else {
+                        data.add("");
+                    }
+                }
+                break;
+
+            case INT64:
+                {
+                    long l = 0;
+                    if (!xcol.getNullmap(row)) {
+                        l = xcol.getI64Data(row);
+                        data.add(Long.toString(l));
+                    } else {
+                        data.add("");
+                    }
+                }
+                break;
+
+            case FLOAT:
+                {
+                    float f = 0;
+                    if (!xcol.getNullmap(row)) {
+                        f = xcol.getF32Data(row);
+                        data.add(Float.toString(f));
+                    } else {
+                        data.add("");
+                    }
+                }
+                break;
+
+            case DOUBLE:
+                {
+                    double d = 0;
+                    if (!xcol.getNullmap(row)) {
+                        d = xcol.getF64Data(row);
+                        data.add(Double.toString(d));
+                    } else {
+                        data.add("");
+                    }
+                }
+                break;
+
+            case DATE:
+            case TIME_MILLIS:
+                {
+                    if (!xcol.getNullmap(row)) {
+                        int date = xcol.getI32Data(row);
+                        data.add(Integer.toString(date));
+                    } else {
+                        data.add("");
+                    }
+                }
+                break;
+
+            case TIME_MICROS:
+            case TIMESTAMP_MILLIS:
+            case TIMESTAMPTZ_MILLIS:
+            case TIMESTAMP_MICROS:
+            case TIMESTAMPTZ_MICROS:
+                {
+                    if (!xcol.getNullmap(row)) {
+                        long date = xcol.getI64Data(row);
+                        data.add(Long.toString(date));
+                    } else {
+                        data.add("");
+                    }   
+                }
+                break;
+            default:
+                {
+                    String s = xcol.getSdata(row);
+                    data.add(s);
+                }
+                break;
+
+            }
+        }
+
+        csvprinter.printRecord(data);
+        
+    }
+   
 }
--- plugin/java/plugin_csv/src/com/vitessedata/xdrive/csv/Main.java
+++ plugin/java/plugin_csv/src/com/vitessedata/xdrive/csv/Main.java
@@ -1,65 +1,80 @@
 package com.vitessedata.xdrive.csv;
 
 import java.io.*;
+import java.nio.file.*;
 import com.vitessedata.xdrive.Xdrive2; 
 import com.vitessedata.xdrive.XdriveUtil;
 
 public class Main {
-	public static void main(String[] args) { 
-		try {
-			System.setErr(new PrintStream("/tmp/xdrive_csv.log"));
-			XdriveUtil.openXdriveIO();
+    public static void main(String[] args) { 
+        try {
 
-			Xdrive2.OpSpec opspec = XdriveUtil.readOpSpec();
+            System.setErr(new PrintStream("/tmp/xdrive_csv.log"));                    
+            if (args.length != 1) {
+                System.err.println("usage: java com.vitessedata.xdrive.csv.Main rootpath");
+                System.exit(1);
+            } 
+            
+            String root_path = args[0];
+            Path rpath = FileSystems.getDefault().getPath(root_path);
+            if (! rpath.isAbsolute()) {
+                System.err.println("rootpath must be absolute");
+                System.exit(1);
+            }
 
-                        Csv csv = new Csv();
-                        
-			switch (opspec.getOp()) {
-				case "read":
-                                    XdriveUtil.replyOpStatus(0, "");
-                                    Xdrive2.ReadRequest rreq = XdriveUtil.readReadRequest();
-                                    csv.read(rreq);
-                                    break;
-
-				case "size_meta":
-                                    XdriveUtil.replyOpStatus(0, "");
-                                    Xdrive2.SizeMetaRequest szreq = XdriveUtil.readSizeMetaRequest();
-                                    csv.sizeMeta(szreq);                                    
-                                    break;
-
-				case "write": 
-					XdriveUtil.replyOpStatus(0, "");
-					Xdrive2.WriteRequest wreq = XdriveUtil.readWriteRequest();
-					XdriveUtil.replyWrite(0, "");
-					csv.writeRequest(wreq);
-
-					boolean done = false;
-					while (!done) { 
-						int errcode = 0;
-						Xdrive2.XCol col = XdriveUtil.readXCol(); 
-						if (col == null || col.getNrow() == 0) { 
-							errcode = csv.write(null);
-							done = true;
-						} else {
-							errcode = csv.write(col);
-						}
-
-						if (errcode != 0) {
-							XdriveUtil.replyWrite(errcode, "Geode write error");
-							break;
-						} else {
-							XdriveUtil.replyWrite(0, "");
-						}
-					}
-					break;
-
-				default:
-                                    XdriveUtil.replyOpStatus(-1, "csv plugin unknown plugin op " + opspec.getOp()); 
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			System.exit(1);
-		}
-	}
+            XdriveUtil.openXdriveIO();
+                
+            Xdrive2.OpSpec opspec = XdriveUtil.readOpSpec();
+            
+            Csv csv = new Csv();
+            csv.config(rpath.toString());
+                
+            switch (opspec.getOp()) {
+            case "read":
+                XdriveUtil.replyOpStatus(0, "");
+                Xdrive2.ReadRequest rreq = XdriveUtil.readReadRequest();
+                csv.read(rreq);
+                break;
+                
+            case "size_meta":
+                XdriveUtil.replyOpStatus(0, "");
+                Xdrive2.SizeMetaRequest szreq = XdriveUtil.readSizeMetaRequest();
+                csv.sizeMeta(szreq);                                    
+                break;
+                
+            case "write": 
+                XdriveUtil.replyOpStatus(0, "");
+                Xdrive2.WriteRequest wreq = XdriveUtil.readWriteRequest();
+                XdriveUtil.replyWrite(0, "");
+                csv.writeRequest(wreq);
+                
+                boolean done = false;
+                while (!done) { 
+                    int errcode = 0;
+                    Xdrive2.XCol col = XdriveUtil.readXCol(); 
+                    if (col == null || col.getNrow() == 0) { 
+                        errcode = csv.write(null);
+                        done = true;
+                    } else {
+                        errcode = csv.write(col);
+                    }
+                    
+                    if (errcode != 0) {
+                        XdriveUtil.replyWrite(errcode, "Csv write error");
+                        break;
+                    } else {
+                        XdriveUtil.replyWrite(0, "");
+                    }
+                }
+                break;
+                
+            default:
+                XdriveUtil.replyOpStatus(-1, "csv plugin unknown plugin op " + opspec.getOp()); 
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+    }
 }
 
